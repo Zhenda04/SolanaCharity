f6ecdcf358f2f1a58a825b1d00302e9ecaad189e
Marlon-id <143787591+Marlon-id@users.noreply.github.com>
2023-09-29 11:41:23 +0800

    Solved: Error Transferring SOL

--- pages/index.js
+++ pages/index.js
@@ -1,160 +1,112 @@
-import { Inter } from 'next/font/google'
-import { useWalletTokenBalance } from '@lndgalante/solutils';
+import { Inter } from 'next/font/google';
+import React, { useState, useEffect } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { useWalletTokenBalance } from '@lndgalante/solutils';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { Connection, Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
-import { Transaction,  } from "@solana/web3.js";
-import { useState } from "react";
-import dynamic from 'next/dynamic'
-import Header from '@/components/Header'
-import Button from '@/components/Button'
+import dynamic from 'next/dynamic';
+import Header from '@/components/Header';
+import Button from '@/components/Button';
 
+import { PublicKey, Transaction, sendAndConfirmTransaction, SystemProgram } from '@solana/web3.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
-function Home() { 
-  const { publicKey } = useWallet();
+function Home() {
+  const { publicKey, wallet } = useWallet();
   const { connection } = useConnection();
-  //New
-  const [txid, setTxid] = useState("");
-  const [recipient, setRecipient] = useState("");
-  const [amountToTransfer, setAmountToTransfer] = useState(0);
-  const [isSending, setIsSending] = useState(false);
-
-  const keypair = Keypair.generate(); // Generate a new keypair
-
-
-  //Hardcoded Recipient
-  const hardcodedRecipient = "BStZkRJAzUXroTFqbEHyCD4uh5boTLF42BubrJwh1hx7";
-
-  const onClickTransfer = async () => {
-    if (!publicKey) return;
-
-    try {
-      new PublicKey(hardcodedRecipient);
-    } catch (error) {
-      alert("Invalid Public Key");
-      return;
-    }
-
-    setIsSending(true);
-
-    try {
-      const ix = SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey(hardcodedRecipient),
-        lamports: amountToTransfer,
-      });
-
-      const { blockhash } = await connection.getLatestBlockhash();
-      const transaction = new Transaction().add(ix); // Create a Transaction
-      transaction.recentBlockhash = blockhash; // Set the recent blockhash
-      transaction.sign(keypair); // Sign the transaction with the keypair
-      const txid = await connection.sendTransaction(transaction); // Send the transaction
-      setTxid(txid);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsSending(false);
-    }
-  }
 
   // solutils hooks
   const { getWalletTokenBalance, result, status, error } = useWalletTokenBalance(publicKey, connection);
+
+  // State to store the recipient's public key and amount for sending SOL
+  const [recipientPublicKey, setRecipientPublicKey] = useState('');
+  const [sendAmount, setSendAmount] = useState(0);
 
   // handlers
   function handleWalletBalanceRequest() {
     getWalletTokenBalance('SOL');
   }
 
+  async function sendSOLToRecipient() {
+    if (!recipientPublicKey || sendAmount <= 0) {
+      alert('Please provide a valid recipient and amount.');
+      return;
+    }
+
+    try {
+      // Convert the recipient's public key to a PublicKey object
+      const receiver = new PublicKey(recipientPublicKey);
+
+      // Create a new transaction
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: receiver,
+          lamports: sendAmount * 10 ** 9, // Convert SOL to lamports
+        })
+      );
+
+      // Sign the transaction
+      const signature = await wallet.signTransaction(transaction);
+
+      // Send the transaction and confirm it
+      const txHash = await connection.sendRawTransaction(signature.serialize());
+      await connection.confirmTransaction(txHash);
+
+      // Transaction was successful
+      alert(`Transaction successful. Transaction hash: ${txHash}`);
+    } catch (err) {
+      console.error(`Error sending SOL: ${err}`);
+      alert(`Error sending SOL: ${err}`);
+    }
+  }
+
   return (
     <main className='flex min-h-screen flex-col p-10 '>
-        <Header/>
-        <div className="relative ">
-          <div className="absolute right-0 top-3">
-          
+      <Header />
+      <div className="relative ">
+        <div className="absolute right-0 top-3">
           <WalletMultiButton />
-          </div>
         </div>
-        <div className="">
-          <h1 className='font-900 text-xl font-sans'>
-            Solana SOL Checker
-          </h1>
-          {publicKey ? 
+      </div>
+      <div className="">
+        <h1 className='font-900 text-xl font-sans'>
+          Solana SOL Checker
+        </h1>
+        {publicKey ? 
           <div className='place-items-center grid mt-10'>
             <Button onClick={handleWalletBalanceRequest}>Request Wallet Balance</Button>
-            {status === 'iddle' ? <p>Haven&apos;t requested any SOL balance yet</p> : null}
+            {status === 'idle' ? <p>Haven't requested any SOL balance yet</p> : null}
             {status === 'loading' ? <p>Requesting your SOL balance tokens</p> : null}
             {status === 'success' ? <p>We successfully got your balance: {result} SOL</p> : null}
             {status === 'error' ? <p>{error}</p> : null}
           </div> : null}
       </div>
 
-      <div>
-      <div className='mt-6'>
-        <p className='font-semibold'>Transfer SOL</p>
-        <div className='mt-4'>
-          Recipient:{""}
+      <div className='mt-4'>
+        <h2 className='font-900 text-lg font-sans'>Send SOL to Another Address</h2>
+        <div className='grid grid-cols-2 gap-4'>
           <input
-            value={hardcodedRecipient}
-            className='text-black rounded-lg border border-black/10 px-2 py-1 w-full max-w-[480px]'
-            onChange={(e) => {
-              setRecipient(e.target.value);
-            }}
+            type='text'
+            placeholder="Recipient's Public Key"
+            value={recipientPublicKey}
+            onChange={(e) => setRecipientPublicKey(e.target.value)}
+            className='p-2 border border-gray-300 rounded'
           />
-        </div>
-        <div className='mt-4'>
-          Amount to transfer:{" "}
           <input
-            className='text-black rounded-lg border border-black-10 px-2 py-1'
-            value={amountToTransfer}
-            onChange={(e) => {
-              setAmountToTransfer(e.target.valueAsNumber);
-            }}
             type='number'
+            placeholder='Amount SOL'
+            value={sendAmount}
+            onChange={(e) => setSendAmount(e.target.value)}
+            className='p-2 border border-gray-300 rounded'
           />
         </div>
-
-        {isSending ? (
-          <button
-            type='button'
-            disabled
-            className='text-black backdrop-blur-2xl rounded-xl px-4 py-2 bg-white mt-4 cursor-not-allowed opacity-50'
-          >
-            Sending...
-          </button>
-        ) : (
-          <button
-            type='button'
-            onClick={onClickTransfer}
-            className='text-black backdrop-blur-2xl rounded-xl px-4 py-2 border-black mt-4'
-          >
-            Transfer
-          </button>
-        )}
-      </div>
-
-      <div className='mt-6'>
-        <p className='font-semibold'>Transaction ID:</p>
-        <div className='mt-4 text-xs'>{txid}</div>
-
-        <div className='flex mt-4'>
-          {txid ? (
-            <a
-              href={`https://explorer.solana.com/tx/${txid}?cluster=devnet`}
-              target='_blank'
-              className='text-black backdrop-blur-2xl rounded-xl px-4 py-2 bg-white'
-            >
-              Open Explorer
-            </a>
-          ) : null}
-        </div>
-      </div>
-
+        <Button onClick={sendSOLToRecipient} className='mt-2'>
+          Send SOL
+        </Button>
       </div>
     </main>
   )
 }
 
-
-export default dynamic (() => Promise.resolve(Home), {ssr: false} )
+export default dynamic(() => Promise.resolve(Home), { ssr: false })
